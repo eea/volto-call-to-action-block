8e1a17f2f622e501348697b1d80216d1219c3c4e
Alin Voinea <contact@avoinea.com>
2022-05-30 22:28:04 +0300

    Add button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+import Edit from './components/Edit';
+import View from './components/View';
+import circleMenuSVG from '@plone/volto/icons/circle-menu.svg';
+import './theme/main.less';
+
 const applyConfig = (config) => {
+  config.blocks.blocksConfig.callToActionBlock = {
+    availableColors: ['#ebebeb', '#023d6b', '#ddeeff', '#eb5f73', '#b9d25f'],
+    id: 'callToActionBlock',
+    title: 'Call to Action',
+    icon: circleMenuSVG,
+    group: 'common',
+    view: View,
+    edit: Edit,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
+  };
   return config;
 };
 
