b80ff96346b62a26d6d7bd5884ef9aad5077d78b
Alin Voinea <contact@avoinea.com>
2023-01-20 12:11:04 +0200

    Add support for mailto: tel:... - refs #159823 (#9)
    * Copy UrlWidget unchanged from @plone/volto 16.8.1
    * feat(CallToAction): Add support for mailto: tel:... - refs #159823

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import callToActionSVG from '@plone/volto/icons/circle-right.svg';
 import Edit from './components/Edit';
 import View from './components/View';
+import Widget from './components/Widget';
 import { StylingSchema, EditSchema } from './components/Schema';
 
 import './theme/main.less';
 
 const applyConfig = (config) => {
+  // Custom Call to Action URL Widget
+  config.widgets.widget.call_to_action = Widget;
+
+  // Call to Action Block
   config.blocks.blocksConfig.callToActionBlock = {
     id: 'callToActionBlock',
     title: 'Call to Action',
@@ -23,6 +28,7 @@ const applyConfig = (config) => {
       view: [],
     },
   };
+
   return config;
 };
 
